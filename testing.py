6e7d0f02154887611b64c2ad085ebac973de8f7d
Glenny Chudra <glentong0@gmail.com>
2023-05-15 11:36:45 +0700

    UP
    UP

--- testing.py
+++ testing.py
@@ -949,8 +949,8 @@ def tp():
     df_new8['Nilai Likelihood Risiko Inheren'] = df_new8['Nilai Likelihood (Risiko Residu)'] - 0.3
     
      #data5
-    df_new9 = df.loc[ (df['KPI'] == 'Information Capital readiness') & (df['Unit'] == 'TP')]
-    df_new10 = df.loc[ (df['KPI'] == 'Information Capital readiness') & (df['Unit'] == 'TP')]
+    df_new9 = df.loc[ (df['KPI'] == 'Progress Billing') & (df['Unit'] == 'TP')]
+    df_new10 = df.loc[ (df['KPI'] == 'Progress Billing') & (df['Unit'] == 'TP')]
     df_new10 = df_new2.drop(['Nilai Consequence Risiko Inheren', 'Nilai Likelihood Risiko Inheren'], axis='columns')
     df_new9['Nilai Likelihood Risiko Inheren'] = df_new9['Nilai Likelihood Risiko Inheren'] - 0.1
     df_new9['Nilai Consequence Risiko Inheren'] = df_new9['Nilai Consequence Risiko Inheren'] - 0.1
@@ -958,13 +958,13 @@ def tp():
     df_new10['Nilai Likelihood Risiko Inheren'] = df_new10['Nilai Likelihood (Risiko Residu)'] - 0.1
     
      #data6
-    df_new9 = df.loc[ (df['KPI'] == 'Progress Billing') & (df['Unit'] == 'TP')]
-    df_new10 = df.loc[ (df['KPI'] == 'Progress Billing') & (df['Unit'] == 'TP')]
-    df_new10 = df_new2.drop(['Nilai Consequence Risiko Inheren', 'Nilai Likelihood Risiko Inheren'], axis='columns')
-    df_new9['Nilai Likelihood Risiko Inheren'] = df_new9['Nilai Likelihood Risiko Inheren'] - 0.25
-    df_new9['Nilai Consequence Risiko Inheren'] = df_new9['Nilai Consequence Risiko Inheren'] - 0.25
-    df_new10['Nilai Consequence Risiko Inheren'] = df_new10['Nilai Consequence (Risiko Residu)'] - 0.25
-    df_new10['Nilai Likelihood Risiko Inheren'] = df_new10['Nilai Likelihood (Risiko Residu)'] - 0.25
+    df_new11 = df.loc[ (df['KPI'] == 'Information Capital readiness') & (df['Unit'] == 'TP')]
+    df_new11 = df.loc[ (df['KPI'] == 'Information Capital readiness') & (df['Unit'] == 'TP')]
+    df_new12 = df_new2.drop(['Nilai Consequence Risiko Inheren', 'Nilai Likelihood Risiko Inheren'], axis='columns')
+    df_new11['Nilai Likelihood Risiko Inheren'] = df_new11['Nilai Likelihood Risiko Inheren'] - 0.25
+    df_new11['Nilai Consequence Risiko Inheren'] = df_new11['Nilai Consequence Risiko Inheren'] - 0.25
+    df_new12['Nilai Consequence Risiko Inheren'] = df_new12['Nilai Consequence (Risiko Residu)'] - 0.25
+    df_new12['Nilai Likelihood Risiko Inheren'] = df_new12['Nilai Likelihood (Risiko Residu)'] - 0.25
     
 
   
