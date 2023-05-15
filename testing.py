2b231b50b9d47f38c3a6576dcdeed381760a9542
Glenny Chudra <glentong0@gmail.com>
2023-05-15 09:58:05 +0700

    con6 fix
    con6

--- testing.py
+++ testing.py
@@ -561,7 +561,7 @@ def th():
     sns.scatterplot(x='Nilai Consequence Risiko Inheren' , y='Nilai Likelihood Risiko Inheren', data=con4,
                     style='Risk', hue='Risk', ax=ax, s=160, palette=["C3", "C3"])
     sns.scatterplot(x='Nilai Consequence Risiko Inheren' , y='Nilai Likelihood Risiko Inheren', data=con5,
-                    style='Risk', hue='Risk', ax=ax, s=160, palette=["C4", "C4"])\
+                    style='Risk', hue='Risk', ax=ax, s=160, palette=["C4", "C4"])
     sns.scatterplot(x='Nilai Consequence Risiko Inheren' , y='Nilai Likelihood Risiko Inheren', data=con6,
                     style='Risk', hue='Risk', ax=ax, s=160, palette=["C2", "C2"])
 
